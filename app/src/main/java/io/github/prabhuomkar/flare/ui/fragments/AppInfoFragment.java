3fc5b497b88922702a7b3f514ba29a948ccb74bd
prabhuomkar <prabhuomkar@pm.me>
2020-03-30 08:35:11 +0530

    Added OSS Licenses information

--- app/src/main/java/io/github/prabhuomkar/flare/ui/fragments/AppInfoFragment.java
+++ app/src/main/java/io/github/prabhuomkar/flare/ui/fragments/AppInfoFragment.java
@@ -1,11 +1,15 @@
 package io.github.prabhuomkar.flare.ui.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity;
 
 import io.github.prabhuomkar.flare.R;
 
@@ -13,6 +17,13 @@ public class AppInfoFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_app_info, container, false);
+        View appInfoView = inflater.inflate(R.layout.fragment_app_info, container, false);
+
+        TextView licensesView = (TextView) appInfoView.findViewById(R.id.app_info_licenses);
+        licensesView.setOnClickListener(v -> {
+            startActivity(new Intent(getActivity(), OssLicensesMenuActivity.class));
+        });
+
+        return appInfoView;
     }
 }
