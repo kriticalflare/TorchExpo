e9ea22edb2504d1996b7d06d0671cb2b212a55e2
Dylan Dsouza <dsouzadyn@gmail.com>
2020-05-31 22:36:12 +0530

    add main activity (#5)

---
+++ app/src/main/java/io/github/prabhuomkar/torchexpo/MainActivity.kt
@@ -0,0 +1,18 @@
+package io.github.prabhuomkar.torchexpo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import io.github.prabhuomkar.torchexpo.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/io/github/prabhuomkar/torchexpo/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package io.github.prabhuomkar.torchexpo.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
