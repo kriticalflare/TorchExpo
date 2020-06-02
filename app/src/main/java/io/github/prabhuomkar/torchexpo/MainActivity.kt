da4e0d11df28ab6e1eb9628c083a222bde6ff2c7
Dylan Dsouza <dsouzadyn@gmail.com>
2020-06-02 22:16:44 +0530

    Add about screen (#8)
    * add navigation architecture component and set home navigation to mainfragment
    
    * Add colors and material components
    
    * add about screen
    
    * Update strings.xml
    
    * add fonts
    
    * update about screen
    
    * Update constraints in about_fragment.xml

--- app/src/main/java/io/github/prabhuomkar/torchexpo/MainActivity.kt
+++ app/src/main/java/io/github/prabhuomkar/torchexpo/MainActivity.kt
@@ -1,12 +1,45 @@
 package io.github.prabhuomkar.torchexpo
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupActionBarWithNavController
+import io.github.prabhuomkar.torchexpo.ui.main.MainFragment
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+        setupNavigation()
+    }
+
+    private fun setupNavigation() {
+        val navController = findNavController(R.id.nav_host_fragment)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        MenuInflater(applicationContext).inflate(R.menu.options_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
---
+++ app/src/main/java/io/github/prabhuomkar/torchexpo/ui/about/AboutFragment.kt
@@ -0,0 +1,41 @@
+package io.github.prabhuomkar.torchexpo.ui.about
+
+import android.content.pm.PackageInfo
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import io.github.prabhuomkar.torchexpo.BuildConfig
+import io.github.prabhuomkar.torchexpo.R
+
+
+class AboutFragment : Fragment() {
+
+    companion object {
+        fun newInstance() =
+            AboutFragment()
+    }
+
+    private lateinit var viewModel: AboutViewModel
+    private lateinit var versionTextView: TextView
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view: View = inflater.inflate(R.layout.about_fragment, container, false)
+        versionTextView = view.findViewById<TextView>(R.id.versionTextView)
+        versionTextView.text = "v${BuildConfig.VERSION_NAME}"
+        return view
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(AboutViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
