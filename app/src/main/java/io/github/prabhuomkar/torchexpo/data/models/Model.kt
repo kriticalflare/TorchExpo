a727e3e63e32ef4042339be1011ef414e73c86f8
kriticalflare <42350771+kriticalflare@users.noreply.github.com>
2020-06-19 18:45:18 +0530

    Change model fields || Add typeconverters and foreign key relationship

--- app/src/main/java/io/github/prabhuomkar/torchexpo/data/models/Model.kt
+++ app/src/main/java/io/github/prabhuomkar/torchexpo/data/models/Model.kt
@@ -1,17 +1,25 @@
 package io.github.prabhuomkar.torchexpo.data.models
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.ForeignKey.CASCADE
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "models")
+@Entity(
+    tableName = "models", foreignKeys = [ForeignKey(
+        entity = Task::class, parentColumns = ["id"], childColumns = ["taskId"],
+        onDelete = CASCADE
+    )]
+)
 data class Model(
     @PrimaryKey
+    val id: Int,
     val name: String,
     val description: String,
     val paperLink: String,
     val sourceLink: String,
     val downloadLink: String,
     val imageLink: String,
-    val playgroundActivity: String,
-    val size: Int
+    val size: Int,
+    val taskId: Int
 )
--- app/src/main/java/io/github/prabhuomkar/torchexpo/data/models/Task.kt
+++ app/src/main/java/io/github/prabhuomkar/torchexpo/data/models/Task.kt
@@ -2,11 +2,42 @@ package io.github.prabhuomkar.torchexpo.data.models
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import androidx.room.TypeConverter
+import androidx.room.TypeConverters
 
 @Entity(tableName = "tasks")
+@TypeConverters(ResearchAreaTypeConverter::class)
 data class Task(
     @PrimaryKey
+    val id: Int,
     val name: String,
     val description: String,
-    val imageLink: String
+    val imageLink: String,
+    val researchArea: ResearchArea
 )
+
+enum class ResearchArea {
+    Vision,
+    Generative,
+    Language,
+    Audio,
+    Unknown
+}
+
+class ResearchAreaTypeConverter {
+    @TypeConverter
+    fun fromResearchArea(researchArea: ResearchArea): String {
+        return researchArea.toString()
+    }
+
+    @TypeConverter
+    fun toResearchArea(area: String): ResearchArea {
+        return when (area) {
+            "Vision" -> ResearchArea.Vision
+            "Generative" -> ResearchArea.Generative
+            "Language" -> ResearchArea.Language
+            "Audio" -> ResearchArea.Audio
+            else -> ResearchArea.Unknown
+        }
+    }
+}
